e1efb1420e3011cc28ffb59f6d235e38adb57ab2
enestalayy <enestalayy@gmail.com>
2024-01-12 18:36:55 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center justify-center">
-    <h1 class="text-xl mb-2 w-4/5 text-center">
+    <h1 class="text-xl my-5 w-4/5 text-center">
       This website is built with its own rendering method for each page in terms of rendering strategies. You can explore my Medium article related to this topic for further insights.
     </h1>
     <div class="sk-ww-medium-post w-1/2 bg-transparent" data-embed-id="25347316"></div>
