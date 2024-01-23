4f25e0c0ebb2fe2caaa970315ec1ab6df6c037db
enestalayy <enestalayy@gmail.com>
2024-01-23 13:55:08 +0300

    Update isr.vue

--- pages/isr.vue
+++ pages/isr.vue
@@ -2,35 +2,35 @@
 definePageMeta({
     layout: 'currency'
 })
-const currencies = ref([
-    { name: '₺ TRY', code: 'TRY', symbol: '₺', value: 0 },
-    { name: '$ USD', code: 'USD', symbol: '$', value: 0 },
-    { name: '€ Euro', code: 'EUR', symbol: '€', value: 0 },
-    { name: '£ GBP', code: 'GBP', symbol: '£', value: 0 },
-])
-const { data, error} = await useFetch('http://api.freecurrencyapi.com/v1/latest?apikey=fca_live_JarnIfUap3XwqgJiUpO961NLR4ZrpNZiIWUCDfdj&currencies=EUR%2CUSD%2CGBP%2CTRY&base_currency=TRY', {
-    key: 'currencies'
-    })
-    if (data) {
+// const currencies =  [
+//     { name: '₺ TRY', code: 'TRY', symbol: '₺', value: 0 },
+//     { name: '$ USD', code: 'USD', symbol: '$', value: 0 },
+//     { name: '€ Euro', code: 'EUR', symbol: '€', value: 0 },
+//     { name: '£ GBP', code: 'GBP', symbol: '£', value: 0 },
+// ]
+// const { data, error} = await useFetch('http://api.freecurrencyapi.com/v1/latest?apikey=fca_live_JarnIfUap3XwqgJiUpO961NLR4ZrpNZiIWUCDfdj&currencies=EUR%2CUSD%2CGBP%2CTRY&base_currency=TRY', {
+//     key: 'currencies'
+//     })
+//     if (data) {
         
-        currencies.value.forEach(currency => {
-            console.log(data.value.data[currency.code])
-            currency.value = 1 / data.value.data[currency.code];
-        });
-        console.log('fetch çalıştı');
-    } else {
-        console.error('Hata:', error);
-    }
-
+//         currencies.value.forEach(currency => {
+//             console.log(data.value.data[currency.code])
+//             currency.value = 1 / data.value.data[currency.code];
+//         });
+//         console.log('fetch çalıştı');
+//     } else {
+//         console.error('Hata:', error);
+//     }
+const currencies = {
+    TRY: 0,
+    USD: 0,
+    EUR: 0,
+    GBP: '21'
+}
 </script>
 
 <template>
-    <div>
-    {{ currencies }}
-    </div>
+    {{ JSON.stringify(currencies) }}
 </template>
-<style>
-div {
-    color: black;
-}</style>
+
 
