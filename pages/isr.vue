3cee6d2ef5df0147193a3532cc53f630286c7b0b
enestalayy <enestalayy@gmail.com>
2024-01-23 18:12:47 +0300

    Update isr.vue

--- pages/isr.vue
+++ pages/isr.vue
@@ -2,13 +2,12 @@
 definePageMeta({
     layout: 'currency'
 })
-const currencies =  {
-    "currencies": [
-    { name: '₺ TRY', code: 'TRY', symbol: '₺', value: 0 },
-    { name: '$ USD', code: 'USD', symbol: '$', value: 0 },
-    { name: '€ Euro', code: 'EUR', symbol: '€', value: 0 },
-    { name: '£ GBP', code: 'GBP', symbol: '£', value: 0 },
-]}
+const currencies = [
+    { code: 'TRY', value: null },
+    { code: 'USD', value: null },
+    { code: 'EUR', value: null },
+    { code: 'GBP', value: null },
+]
 const { data, error} = await useFetch('http://api.freecurrencyapi.com/v1/latest?apikey=fca_live_JarnIfUap3XwqgJiUpO961NLR4ZrpNZiIWUCDfdj&currencies=EUR%2CUSD%2CGBP%2CTRY&base_currency=TRY', {
     key: 'currencies'
     })
