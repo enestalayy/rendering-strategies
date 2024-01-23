253cee151c9aaa9835861d08eeef97901a152251
enestalayy <enestalayy@gmail.com>
2024-01-23 17:18:44 +0300

    Update isr.vue

--- pages/isr.vue
+++ pages/isr.vue
@@ -9,19 +9,18 @@ const currencies =  {
     { name: '€ Euro', code: 'EUR', symbol: '€', value: 0 },
     { name: '£ GBP', code: 'GBP', symbol: '£', value: 0 },
 ]}
-// const { data, error} = await useFetch('http://api.freecurrencyapi.com/v1/latest?apikey=fca_live_JarnIfUap3XwqgJiUpO961NLR4ZrpNZiIWUCDfdj&currencies=EUR%2CUSD%2CGBP%2CTRY&base_currency=TRY', {
-//     key: 'currencies'
-//     })
-//     if (data) {
-        
-//         currencies.value.forEach(currency => {
-//             console.log(data.value.data[currency.code])
-//             currency.value = 1 / data.value.data[currency.code];
-//         });
-//         console.log('fetch çalıştı');
-//     } else {
-//         console.error('Hata:', error);
-//     }
+const { data, error} = await useFetch('http://api.freecurrencyapi.com/v1/latest?apikey=fca_live_JarnIfUap3XwqgJiUpO961NLR4ZrpNZiIWUCDfdj&currencies=EUR%2CUSD%2CGBP%2CTRY&base_currency=TRY', {
+    key: 'currencies'
+    })
+    if (data) {
+        currencies.value.forEach(currency => {
+            console.log(data.value.data[currency.code])
+            currency.value = 1 / data.value.data[currency.code];
+        });
+        console.log('fetch çalıştı');
+    } else {
+        console.error('Hata:', error);
+    }
 
 </script>
 
