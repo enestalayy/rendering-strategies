0abb20cae3e9304c8afd83df5181e1f9fba1f5fb
enestalayy <enestalayy@gmail.com>
2024-01-23 17:18:27 +0300

    Update isr.vue

--- pages/isr.vue
+++ pages/isr.vue
@@ -2,12 +2,13 @@
 definePageMeta({
     layout: 'currency'
 })
-// const currencies =  [
-//     { name: '₺ TRY', code: 'TRY', symbol: '₺', value: 0 },
-//     { name: '$ USD', code: 'USD', symbol: '$', value: 0 },
-//     { name: '€ Euro', code: 'EUR', symbol: '€', value: 0 },
-//     { name: '£ GBP', code: 'GBP', symbol: '£', value: 0 },
-// ]
+const currencies =  {
+    "currencies": [
+    { name: '₺ TRY', code: 'TRY', symbol: '₺', value: 0 },
+    { name: '$ USD', code: 'USD', symbol: '$', value: 0 },
+    { name: '€ Euro', code: 'EUR', symbol: '€', value: 0 },
+    { name: '£ GBP', code: 'GBP', symbol: '£', value: 0 },
+]}
 // const { data, error} = await useFetch('http://api.freecurrencyapi.com/v1/latest?apikey=fca_live_JarnIfUap3XwqgJiUpO961NLR4ZrpNZiIWUCDfdj&currencies=EUR%2CUSD%2CGBP%2CTRY&base_currency=TRY', {
 //     key: 'currencies'
 //     })
@@ -21,12 +22,7 @@ definePageMeta({
 //     } else {
 //         console.error('Hata:', error);
 //     }
-const currencies = {
-    TRY: 0,
-    USD: 0,
-    EUR: 0,
-    GBP: '21'
-}
+
 </script>
 
 <template>
